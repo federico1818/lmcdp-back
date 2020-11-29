21d877e5716c0c48b7837c89e9728290baec4784
Federico González <federico1818@gmail.com>
2020-11-29 02:42:06 -0300

    add query error

--- resources/lang/es/error.php
+++ resources/lang/es/error.php
@@ -5,7 +5,15 @@ return [
     'Symfony\Component\HttpKernel\Exception\NotFoundHttpException' => [
         'response' => [
             'title' => 'Error de conexión',
-            'message' => 'No es posible conectarse con el servidor en estos momentos'
+            'message' => 'No es posible conectarse con el servidor en estos momentos.'
+        ],
+        'code' => 503
+    ],
+    
+    'Illuminate\Database\QueryException' => [
+        'response' => [
+            'title' => 'Error de conexión',
+            'message' => 'No es posible conectarse con la base de datos.'
         ],
         'code' => 503
     ]
